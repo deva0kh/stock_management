c890451e5f2b14ad069a3f4db804e5b7b36befa4
deva0kh <achraf.kh03@gmail.com>
2021-02-02 22:13:10 +0000

    front end

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:stock_managements/components/my_bottom_nav_bar.dart';
+import 'package:stock_managements/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
